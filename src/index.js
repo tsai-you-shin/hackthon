5610f0671b9a79417ffe88af1ffdd83ec47ace6c
shiannn <b05502087@ntu.edu.tw>
2019-10-19 08:45:54 +0800

    finish pad moving

--- src/index.js
+++ src/index.js
@@ -9,6 +9,32 @@ const GAME_HEIGHT = 600;
 ctx.clearRect(0,0,800,600);
 //畫之前要先清乾淨
 
+class InputHandler{
+    constructor(paddle){
+        document.addEventListener("keydown",event=>{
+            switch(event.keyCode){
+                case 37:
+                    paddle.moveLeft();
+                    break;
+                case 39:
+                    paddle.moveRight();
+                    break;
+            }
+        });
+        document.addEventListener("keyup",event=>{
+            switch(event.keyCode){
+                case 37:
+                    if(paddle.speed<0)paddle.stop();
+                    break;
+                case 39:
+                    if(paddle.speed>0)paddle.stop();
+                    break;
+            }
+        });
+    }
+}
+
+
 
 ctx.fillStyle = '#f00';
 //預定為紅色，下面就都是紅色
@@ -18,27 +44,50 @@ ctx.fillRect(20,20,100,100);
 ctx.fillStyle = '#00f';
 ctx.fillRect(270,200,50,50);
 
+
+
 class Paddle {
     constructor(gameWidth,gameHeight){
         this.width = 150;
         this.height= 20;
+
+        this.maxSpeed = 10;
+        this.speed = 0;
 
         this.position={
             x:gameWidth / 2 - this.width / 2,
             y:gameHeight - this.height - 10
         };
     }
+
+    moveLeft(){
+        this.speed = -this.maxSpeed;
+    }
+    moveRight(){
+        this.speed = +this.maxSpeed;
+    }
+    stop(){
+        this.speed = 0;
+    }
+
     draw(ctx){
         ctx.fillStyle = '#0ff';
         ctx.fillRect(this.position.x,this.position.y,this.width,this.height);
     }   
     update(deltaTime){
         if(!deltaTime) return;
-        this.position.x += 5 / deltaTime;
+        this.position.x += this.speed;
+        if(this.position.x < 0) this.position.x = 0;
+
+        if(this.position.x + this.width > GAME_WIDTH)
+            this.position.x = GAME_WIDTH - this.width;
     }
 }
 
 let paddle = new Paddle(GAME_WIDTH,GAME_HEIGHT);
+
+new InputHandler(paddle);
+
 paddle.draw(ctx);
 
 let lastTime = 0;
